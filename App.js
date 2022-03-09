060cda12f61f9f335d0e4c681f14541428e8e3e4
Lucas <lucas_borba@outlook.com>
2022-03-09 20:57:15 -0300

    Aula Concluida

--- App.js
+++ App.js
@@ -1,31 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Switch, StyleSheet, TouchableOpacity } from 'react-native';
 
 const App = () => {
+
+  const [visible, setVisible] = useState(true);
+  const [saldo, setSaldo] = useState(0, 0);
+  const [valorUsuario, setValorUsuario] = useState('');
+
+  const add = () => {
+
+    if (valorUsuario) {
+      const temp = parseFloat(valorUsuario);
+      setSaldo(saldo + temp);
+      setValorUsuario('');
+    } else {
+      alert('Digite o valor !!!')
+    }
+  };
+
+  const remove = () => {
+
+    if (valorUsuario) {
+      const temp = parseFloat(valorUsuario);
+      setSaldo(saldo - temp);
+      setValorUsuario('');
+    } else {
+      alert('Digite o valor !!!')
+    }
+  };
+
   return (
     <View style={{ padding: 10 }}>
       <Text style={{ marginBottom: 20 }} >Carteira Digital</Text>
 
-      <View style={{ backgroundColor: 'blue', padding: 5, borderRadius: 10 }}>
-
-        <View styles={styles.view1}>
+      <View style={{ backgroundColor: 'blue', padding: 10, borderRadius: 10 }}>
+        <View style={styles.view1}>
           <Text>Seu Saldo</Text>
-          <Switch />
-          <Text style={styles.saldo} >R$ 500,00</Text>
+          <Switch value={visible} onValueChange={()=>setVisible(!visible) }/>
+        </View>
 
-          <TextInput style={{ backgroundColor: 'white', marginVertical: 10 }} />
+        {visible ? (
+          <Text style={styles.saldo}>R$ {saldo}</Text>
+        ) : (
+          <Text style={styles.saldo}>R$--</Text>
+        )}
 
-          <View style={styles.view2} >
-            <TouchableOpacity style={styles.button}>
-              <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 16 }} >Adicionar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 16 }}>Remover</Text>
-            </TouchableOpacity>
-          </View>
+        <TextInput
+          value={valorUsuario}
+          onChangeText={(value) => setValorUsuario(value)}
+          style={{ backgroundColor: 'white', marginVertical: 10 }}
+        />
+
+        <View style={styles.view2} >
+          <TouchableOpacity onPress={add} style={styles.button}>
+            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 16 }} >
+              Adicionar
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={remove} style={styles.button}>
+            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 16 }}>
+              Remover
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
+
   );
 };
 
@@ -34,16 +75,19 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+
   saldo: {
     color: 'white',
     fontSize: 30,
     textAlign: 'center',
     marginTop: 10,
   },
+
   view2: {
     justifyContent: 'space-evenly',
     flexDirection: 'row',
   },
+
   button: {
     height: 40,
     justifyContent: 'center',
